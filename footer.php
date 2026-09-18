c54ecc570f1182a9473b1c67a657f64451716581
warchiello <warchiello@gmail.com>
2026-09-18 11:43:18 -0400

    Fix dead space below deck rows by removing fixed-height deck class from footer

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<div name="footer" class="hdcpDeck hdcpDeckBorderlt footer small">
+<div name="footer" class="hdcpDeckBorderlt footer small">
 		<a href="https://hyperdeckpanel.com/" target="_blank">Panel for HyperDeck</a> | <a href="https://hyperdeckpanel.com/github.html" target="_blank">Updates &amp; Support</a> &emsp;&emsp;&emsp;
 		Version <?php echo $installed_version; ?> | PHP <?php echo defined('PHP_VERSION') ? PHP_VERSION : 'version unknown'; ?>
 </div>
